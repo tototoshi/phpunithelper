252d01af1d5597ebef071407779bf922e3cb2ead
toshiyuki <t.toshi.0412@gmail.com>
2013-09-29 00:37:07 +0900

    Fixed generated namespace when namespace is global.

--- src/PHPUnitHelper/Generator/SkeltonGenerator.php
+++ src/PHPUnitHelper/Generator/SkeltonGenerator.php
@@ -85,7 +85,7 @@ class SkeltonGenerator
 
         $namespaceCode = '';
 
-        if ($namespace !== '') {
+        if ($namespace !== 'global') {
             $namespaceCode = "namespace $namespace;\n\n";
         }
 
