dabaa227ebe7834e43629b40098a9e8d03588cc1
toshiyuki <t.toshi.0412@gmail.com>
2013-09-30 01:07:15 +0900

    Added version

--- src/PHPUnitHelper/Application.php
+++ src/PHPUnitHelper/Application.php
@@ -3,7 +3,8 @@ namespace PHPUnitHelper;
 
 require 'vendor/autoload.php';
 
-$app = new \Cilex\Application('phpunithelper');
+$version = '0.1.0-dev';
+$app = new \Cilex\Application('phpunithelper', $version);
 $app->command(new GenerateCommand());
 $app->command(new ConfigCommand());
 $app->run();
